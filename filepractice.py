9c8d7f7487f8d8bdde46fd504a683893470312eb
andrie <ramorSTEM2022@gmail.com>
2022-06-10 21:45:08 -0400

    file practice

--- filepractice.py
+++ filepractice.py
@@ -1,2 +1,17 @@
-f = open('text', 'r')
+import os
+
+f = open('text', 'r') # Reading a file
+# 'r' represents reading mode
 print(f.read())
+
+f = open('text', 'a')
+f.write("new line") # Adds a more text to file
+f.close()
+
+# f = open('text', 'x') # Creates a file. Makes an error if file exists
+f = open('temp', 'x') # Creates file
+os.remove('temp') # deletes file
+if os.path.exists('temp'):
+    os.remove('temp')
+else:
+    print('The file does not exist')
